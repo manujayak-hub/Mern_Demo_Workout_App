095c081a13e8f21304d1de5a1c1590adbdfb12a4
Manujaya Kamburugamuwa <manujayak8@gmail.com>
2024-01-05 14:57:34 +0530

    fix the router issue
    express.Router should be "R" capital

--- Backend/Server.js
+++ Backend/Server.js
@@ -2,18 +2,22 @@ require('dotenv').config()
 
 const express = require ('express')
 
+//taking router module which that exported
+const workoutrouter = require('./routes/workouts')
+
 //express app
 const app = express()
 
 //middleware
+
+app.use(express.json())
+
 app.use((req , res , next) => {
     console.log(req.path,req.method)
     next()
 })
 //routes
-app.get('/',(req , res) => {
-    res.json({mssg: 'Welcome to the app'})
-})
+app.use('/api/workouts',workoutrouter)
 
 //listen for app
 app.listen(process.env.PORT , () => {
--- Backend/routes/workouts.js
+++ Backend/routes/workouts.js
@@ -1,4 +1,32 @@
 const express = require('express')
 
-const router = express.router()
+const router = express.Router()
 
+//get all workout
+router.get('/' , (req , res) => {
+    res.json({mssg: 'Get all Workout'})
+})
+
+//get a single workout
+router.get('/:id' , (req , res) => {
+    res.json({mssg: 'Get a single Workout'})
+})
+
+//post a workout
+router.post('/' , (req , res) => {
+    res.json({mssg: 'post a new Workout'})
+})
+
+//delete a workout
+router.delete('/:id' , (req , res) => {
+    res.json({mssg: 'delete a Workout'})
+})
+
+//update a workout
+router.patch('/' , (req , res) => {
+    res.json({mssg: 'update a Workout'})
+})
+
+//here we export the router module and this will get it as a require
+// in server.js
+module.exports = router
